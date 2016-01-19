7f66514f641440b48bf366524d89481ea79861c9
Soroush Mehraein <soroushmehraein@gmail.com>
2016-01-19 12:12:36 -0800

    Add documentation

--- pgdbGO.go
+++ pgdbGO.go
@@ -1,3 +1,6 @@
+/*
+Utility package for managing Postgres database interactions on a basic Go server.
+*/
 package pgdbGO
 
 import (
@@ -11,7 +14,8 @@ type PGConnection struct {
 	DB_USER, DB_PASSWORD, DB_NAME, DB_HOST string
 }
 
-// Remember to defer db.Close() when you call this
+// Opens a DB connection when passed an appropriate set of parameters.
+// Checks the connection and returns an error if it's faulty.
 func Connect(c PGConnection) (db *sql.DB, err error) {
 	dbinfo := fmt.Sprintf("user=%s password=%s dbname=%s host=%s sslmode=disable", c.DB_USER, c.DB_PASSWORD, c.DB_NAME, c.DB_HOST)
 	db, _ = sql.Open("postgres", dbinfo)
@@ -19,6 +23,7 @@ func Connect(c PGConnection) (db *sql.DB, err error) {
 	return
 }
 
+// Creates a new user in Postgres when provided a db connection and appropriate parameters.
 func CreateNewUser(db *sql.DB, name, password, email, sportsTeam string, gender int) (err error) {
 	var lastInsertId int
 	err = db.QueryRow(`INSERT INTO userinfo(name, password, email, sportsTeam, gender, created) VALUES($1, $2, $3, $4, $5, $6)
