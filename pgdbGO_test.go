819e8c800176fbaef8fc1bafca5a00fbcf462c6b
Soroush Mehraein <soroushmehraein@gmail.com>
2016-01-19 00:19:15 -0800

    Update test suite to check for live connection
    The sql.Open function itself doesn't actually return an error if unable
    to connect. You need to actually try to ping the *DB it gives you and
    see if THAT gives you an error.

--- pgdbGO_test.go
+++ pgdbGO_test.go
@@ -6,10 +6,11 @@ func TestConnect(t *testing.T) {
 	cases := []struct {
 		in PGConnection
 	}{
-		{PGConnection{"postgres", "", "postgres", "postgres"}},
+		{PGConnection{"postgres", "", "postgres", "localhost"}},
 	}
 	for _, c := range cases {
-		conn, err := Connect(c.in)
+		conn, _ := Connect(c.in)
+		err := conn.Ping()
 		if err != nil {
 			t.Errorf("Error connecting to database: %q", err)
 		} else {
